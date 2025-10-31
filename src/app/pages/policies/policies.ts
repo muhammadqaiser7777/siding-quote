c9f017c583626b730e99e889f673b5a9a7d793cb
muhammadqaiser7777 <qaiserakram7777@gmail.com>
2025-10-31 17:07:23 +0500

    Initial Structure

---
+++ src/app/pages/policies/policies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policies',
+  imports: [],
+  templateUrl: './policies.html',
+  styleUrl: './policies.css',
+})
+export class Policies {
+
+}
